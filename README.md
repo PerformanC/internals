9f107dbb68ed49762a943daadb49971cba5b0932
ThePedroo <pedroolimpioguerra@gmail.com>
2024-04-20 18:58:14 -0300

    add: PCTL base
    This commit adds the PCTL library base.

--- README.md
+++ README.md
@@ -1,20 +1,76 @@
-# PerformanC Internals
+# PCTL
 
-This documents has the purpose of listing and explaining the packages and modules that are part of the PerformanC projects.
+Runtime type checking library for JavaScript (PerformanC's Typing Library)
 
-All the packages listed can be find in their branches inside this repository.
+## Features
 
-## Table of contents
+- Lightweight
+- Easy to use
+- Precise type checking
+- No dependencies
 
-- [Packages](#packages)
-  - [PWSL-mini](#pwsl-mini)
+## Dependencies
 
-## Packages
+- N/A
 
-### PWSL-mini
+## Installation
 
-PWSL-mini is a reduced version of the PWSL library, which is a library that provides easy access to the WebSocket protocol within a light package.
+```shell
+$ npm i PerformanC/Internals#PCTL
+```
 
-#### Used in
+## Usage
 
-- [FastLink](https://github.com/PerformanC/FastLink)
+```js
+import verifyParams from '@performanc/pctl'
+
+const checks = {
+  'id': {
+    type: 'string',
+    extraVerification: (param) => {
+      if (param == '0') {
+        console.log('Cannot use id 0')
+
+        process.exit(1)
+      }
+    }
+  },
+  'name': {
+    type: 'string',
+    extraVerification: (param) => {
+      if (param.length < 3) {
+        console.log('Name must be at least 3 characters long')
+
+        process.exit(1)
+      }
+    }
+  }
+}
+
+const options = {
+  'id': '0',
+  'name': 'John'
+}
+
+helper.verifyParams(checks, options)
+```
+
+## Documentation
+
+As an internal library, PCTL does not have a dedicated documentation. You should refer to the source code for more information.
+
+## Support
+
+Any question related to PCTL or other PerformanC projects can be made in [PerformanC's Discord server](https://discord.gg/uPveNfTuCJ).
+
+## Contribution
+
+It is mandatory to follow the PerformanC's [contribution guidelines](https://github.com/PerformanC/contributing) to contribute to PCTL. Following its Security Policy, Code of Conduct and syntax standard.
+
+## Projects using PCTL
+
+- [PerforVNMaker](https://github.com/PerformanC/PerforVNMaker)
+
+## License
+
+PCTL is licensed under [BSD 2-Clause License](LICENSE). You can read more about it on [Open Source Initiative](https://opensource.org/licenses/BSD-2-Clause).
---
+++ index.js
@@ -0,0 +1,116 @@
+function verifyParams(check, params, additionalinfo = {}) {
+  if (params === undefined) {
+    logFatal('The parameters object is undefined.')
+  }
+
+  if (typeof params != 'object') {
+    logFatal('The parameters object must be an object.')
+  }
+
+  const isArray = Array.isArray(params)
+  if (isArray) {
+    params.forEach((item, index) => {
+      additionalinfo.parent = params
+      additionalinfo.index = index
+
+      verifyParams(check, item, additionalinfo)
+    })
+
+    return;
+  }
+
+  Object.keys(check).forEach((key) => {
+    const checkKey = check[key]
+
+    if (checkKey.shouldCheck && !checkKey.shouldCheck(params[key], additionalinfo)) return;
+
+    if (checkKey.required != false && params[key] === undefined) {
+      logFatal(`The parameter "${key}" is required.`)
+    }
+  })
+
+  Object.keys(params).forEach((key) => {
+    const param = params[key]
+    const checkKey = check[key]
+
+    if (!checkKey) {
+      logWarning(`Unecessary parameter "${key}".`)
+
+      return;
+    }
+
+    if (checkKey.shouldCheck && !checkKey.shouldCheck(param, additionalinfo)) return;
+
+    const type = isArray ? 'array' : typeof param
+
+    if (checkKey.params) {
+      if (type != 'object') {
+        logFatal(`The parameter "${key}" must be an object or an array.`)
+      }
+
+      if (isArray == true && param.length == 0 && checkKey.required == true) {
+        logFatal(`The parameter "${key}" must not be empty.`)
+      }
+
+      additionalinfo.parent = params
+
+      verifyParams(checkKey.params, param, additionalinfo)
+    }
+
+    switch (checkKey.type) {
+      case 'file': {
+        try {
+          fs.accessSync((checkKey.basePath || './') + param, fs.constants.F_OK)
+        } catch {
+          logFatal(`The file "${param}" does not exist.`)
+        }
+
+        break
+      }
+      case 'fileInitial': {
+        try {
+          if (!fs.readdirSync((checkKey.basePath || './')).find((file) => {
+            return file.startsWith(param)
+          })) {
+            logFatal(`The file "${param}" does not exist.`)
+          }
+        } catch {
+          logFatal(`The file "${param}" does not exist.`)
+        }
+
+        break
+      }
+      default: {
+        if (!checkKey.type.includes(Array.isArray(param) ? 'array' : typeof param)) {
+          logFatal(`The parameter "${key}" must be a ${checkKey.type}.`)
+        }
+      }
+    }
+
+    if ((checkKey.shouldCheckValues ? checkKey.shouldCheckValues(param, additionalinfo) : 'true') && checkKey.values) {
+      if (!checkKey.values.includes(param)) {
+        logFatal(`The parameter "${key}" must be one of the following values: ${checkKey.values.join(', ')}.`)
+      }
+    }
+
+    if ((checkKey.shouldCheckNotValues ? checkKey.shouldCheckNotValues(param, additionalinfo) : true) && checkKey.notValues) {
+      if (checkKey.notValues.includes(param)) {
+        logFatal(`The parameter "${key}" must not be one of the following values: ${checkKey.notValues.join(', ')}.`)
+      }
+    }
+  
+    if (checkKey.min !== undefined && param < checkKey.min) {
+      logFatal(`The parameter "${key}" must be greater than or equal to ${checkKey.min}.`)
+    }
+
+    if (checkKey.max !== undefined && param > checkKey.max) {
+      logFatal(`The parameter "${key}" must be less than or equal to ${checkKey.max}.`)
+    }
+
+    if (checkKey.extraVerification) {
+      checkKey.extraVerification(param, additionalinfo)
+    }
+  })
+}
+
+export default verifyParams
